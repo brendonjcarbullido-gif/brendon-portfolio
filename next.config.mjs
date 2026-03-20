61ec975ac3086af1e44bd046342a26f2085e4b0f
Brendon Carbullido <brendonjcarbullido@gmail.com>
2026-03-20 04:10:45 -0700

    finalize next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
   transpilePackages: ['@mediapipe/face_mesh', '@mediapipe/camera_utils'],
 }
-
 export default nextConfig

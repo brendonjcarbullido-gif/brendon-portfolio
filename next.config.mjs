1cb11259b212196735f59316d264a3e419a860a4
Brendon Carbullido <brendonjcarbullido@gmail.com>
2026-03-20 02:39:27 -0700

    fix next.config standalone output for docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ['@mediapipe/face_mesh', '@mediapipe/camera_utils'],
 }
 
